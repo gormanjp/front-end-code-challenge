7494c9a6c6207b343b34235f0b2a163ca5bd9f34
gormanjp <jpgorman8@gmail.com>
2018-01-07 20:20:45 -0500

    style updates

--- source/containers/hotels.js
+++ source/containers/hotels.js
@@ -1,33 +1,44 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-class Hotels extends Component {
+const images =
+  [ "//d29u3c1wxehloe.cloudfront.net/vfml/1950/417/63343589/big.jpg",
+   "//d29u3c1wxehloe.cloudfront.net/vfml/1950/417/63343581/big.jpg",
+   "//d29u3c1wxehloe.cloudfront.net/vfml/1086/886/65082242/big.jpg" ];
 
-  renderHotel(hotel, type){
+class Hotels extends Component {
+  renderHotel(hotel, type, photo){
+    const image = `https:${images[photo]}`;
     return(
       <div>
-        <h1>{type.toUpperCase()}</h1>
-        <div>
-          {hotel.name}
+        <h3 className="hotel-category raleway-big">{type.toUpperCase()}</h3>
+        <div className="line"></div>
+        <div className="card hotel-card raleway-big">
+          <img className="card-img-top hotel-img" src={image}/>
+          <div className="card-block">
+            <h4 className="card-title">{hotel.name}</h4>
+            <h5 className="card-text">${hotel.indicative_rate}</h5>
+            <a href={`https://www.roomkey.com/hotels/${hotel.udicode}`} target="_blank" className="btn btn-primary card-btn">BOOK IT!</a>
+          </div>
         </div>
       </div>
+
     )
   }
 
   render(){
     const {cheapestHotels, starsHotels, popularHotels} = this.props;
     return(
-      <div className="row">
-        <div>{cheapestHotels ? this.renderHotel(cheapestHotels[0], 'Cheapest') : ''}</div>
-        <div>{starsHotels ? this.renderHotel(starsHotels[0], 'Most Stars') : ''}</div>
-        <div>{popularHotels ? this.renderHotel(popularHotels[0], 'Most Popular') : ''}</div>
+      <div className="card-deck">
+        <div className="card-container">{starsHotels ? this.renderHotel(starsHotels[0], 'Most Stars', 1) : ''}</div>
+        <div className="card-container">{popularHotels ? this.renderHotel(popularHotels[0], 'Most Popular', 2) : ''}</div>
+        <div className="card-container">{cheapestHotels ? this.renderHotel(cheapestHotels[0], 'Cheapest', 0) : ''}</div>
       </div>
     )
   }
 }
 
 function mapStateToProps({hotels}){
-  debugger
   return {...hotels};
 }
 
